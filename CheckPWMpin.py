ff8b7094006329d77d29552e9cc8d9295ec5886d
Urs Utzinger <utzinger@email.arizona.edu>
2023-09-19 17:19:05 -0700

    Update CheckPWMpin.py

--- CheckPWMpin.py
+++ CheckPWMpin.py
@@ -15,7 +15,7 @@ import logging
 ###########################################################
 
 PWM_PIN       = board.D13 # PWM pin, use same pin for left and right light
-PWM_FREQUENCY = 500       # 100Hz .. 1kHz   
+PWM_FREQUENCY = 500       # 100Hz .. 1kHz
 
 ##############################################################################################
 # MAIN
@@ -23,9 +23,21 @@ PWM_FREQUENCY = 500       # 100Hz .. 1kHz
 
 # Setup logging
 logger = logging.getLogger(__name__)
+log_level = logging.INFO
+logging.basicConfig(
+    level=log_level,
+    format='%(asctime)-15s %(levelname)s: %(message)s'
+)
 logger.log(logging.INFO, 'Starting Flashlight...')
 
-intensity = 25. / 100.
+intensity = 0. / 100.
 pin = pwmio.PWMOut(PWM_PIN, frequency=PWM_FREQUENCY, duty_cycle=0)
 pin.duty_cycle = int(intensity * 65535) 
+
+input("Press Enter to set power to 25% ...")
+
+intensity = 25./100.
+pin.duty_cycle = int(intensity * 65535)
+
+input("Press Enter to exit ...")
 
